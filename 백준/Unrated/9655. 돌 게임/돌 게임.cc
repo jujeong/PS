a228f3b2f27d1fe124b1cab633f178cc2c9514b1
jujeong <44399938+jujeong@users.noreply.github.com>
2023-03-18 04:12:04 +0900

    [Unrated] Title: 돌 게임, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Unrated/9655. 돌 게임/돌 게임.cc
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int N;
+int arr[1001];
+void progress()
+{
+    
+}
+void input()
+{
+    cin >> N;
+}
+void output()
+{
+    if (N % 2)
+    {
+        cout << "SK";
+    }
+    else
+    {
+        cout << "CY";
+    }
+}
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    
+    
+    input();
+    progress();
+    output();
+}
