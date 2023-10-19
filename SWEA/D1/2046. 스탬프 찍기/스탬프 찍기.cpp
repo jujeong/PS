734a69c38fff580c980e59692990355adc241d18
jujeong <44399938+jujeong@users.noreply.github.com>
2023-10-19 19:17:17 +0900

    [D1] Title: 스탬프 찍기, Time: 5 ms, Memory: 13,540 KB -BaekjoonHub

---
+++ SWEA/D1/2046. 스탬프 찍기/스탬프 찍기.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(int argc, char** argv)
+{
+	int n;
+    cin >> n;
+    while(n--)
+    {
+        cout << "#";
+    }
+	return 0;//정상종료시 반드시 0을 리턴해야합니다.
+}
