d97d4e3a3fd858f847da15526b8defacf2dd3e31
JuJeong <44399938+jujeong@users.noreply.github.com>
2024-06-14 16:25:49 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 612 ms, Memory: 852 KB -BaekjoonHub

---
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+    "fmt"
+)
+
+func main() {
+    var N int
+    fmt.Scanf("%d", &N)
+    for i := 1; i <= N; i++ {
+        for j := 1; j <= N - i; j++ {
+            fmt.Printf(" ")
+        }
+        for j := 1; j <= i; j++ {
+            fmt.Printf("*")
+        }
+        fmt.Println()
+    }
+}
