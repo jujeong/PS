fd499f4834d09a5cb7f4d456a4d173541d760b72
JuJeong <44399938+jujeong@users.noreply.github.com>
2024-06-14 16:17:39 +0900

    [Bronze V] Title: A+B - 8, Time: 24 ms, Memory: 872 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+    "fmt"
+)
+
+func main() {
+    var T, A, B int
+    fmt.Scanf("%d", &T)
+    for i := 1; i <= T; i++ {
+        fmt.Scanf("%d %d", &A, &B)
+        fmt.Printf("Case #%d: %d + %d = %d\n", i, A, B, A + B)        
+    }
+}
