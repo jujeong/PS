726a64f2679e17d631ecaf6fd9266adc2423f394
JuJeong <44399938+jujeong@users.noreply.github.com>
2024-06-14 15:54:52 +0900

    [Bronze V] Title: 구구단, Time: 4 ms, Memory: 848 KB -BaekjoonHub

---
+++ 백준/Bronze/2739. 구구단/구구단.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+    "fmt"
+)
+
+func main() {
+    var N int
+    fmt.Scanf("%d", &N)
+    for i := 1; i < 10; i++ {
+        fmt.Printf("%d * %d = %d\n", N, i, N * i)
+    }
+}
